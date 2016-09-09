879bf0471bc6445fa6140e697979e66f8e873f6e
Simon Edwards <s.edwards@ultimaker.com>
2016-09-09 21:23:24 +0200

    Added some scripts to make it easyer to build on my Windows machine.

---
+++ build_electron_module.bat
@@ -0,0 +1,6 @@
+
+npm config set msvs_version 2015
+node scripts/build -f --electron=1.3.5
+
+echo
+echo "Done"
---
+++ setup_build_env.bat
@@ -0,0 +1,6 @@
+
+set PATH=c:\python27\;%PATH%
+"c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+
+echo
+echo "Ready to build"
